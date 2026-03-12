9ac526d7f779984f95a32cd5c6fd74e86ff881bc
Nhue Dang <nhuedangduc3@gmail.com>
2026-03-13 00:35:12 +0700

    blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductListComponent } from './pages/products/product-list.component';
 import { ProductDetailComponent } from './pages/products/product-detail.component';
 import { AboutUsComponent } from './pages/about/about-us.component';
+import { CanvasHangingGuideComponent } from './pages/blog/canvas-hanging-guide.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutUsComponent },
     { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailComponent }
+    { path: 'products/:id', component: ProductDetailComponent },
+    { path: 'blog/huong-dan-treo-tranh-canvas', component: CanvasHangingGuideComponent }
 ];
---
+++ src/app/pages/blog/canvas-hanging-guide.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-canvas-hanging-guide',
+  imports: [CommonModule],
+  templateUrl: './canvas-hanging-guide.component.html',
+  styleUrl: './canvas-hanging-guide.component.css',
+})
+export class CanvasHangingGuideComponent {}
+
